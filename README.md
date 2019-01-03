81df283a6e7e9353fc05fc353df7b3b5ca223e42
Eric-T <erictalkar@gmail.com>
2019-01-02 19:29:31 -0500

    Final commit

--- README.md
+++ README.md
@@ -1,20 +1,41 @@
 # Memory Game Project
 
+This is a classic game of memory, where the player has to match all pairs of cards in the deck. You win the game once all pairs are matched!
+This game was developed using HTML, CSS, and Javascript.
+
 ## Table of Contents
 
 * [Instructions](#instructions)
+* [How the game works](#howthegameworks)
+* [Instructions](#howyourrated)
 * [Contributing](#contributing)
 
 ## Instructions
 
-The starter project has some HTML and CSS styling to display a static version of the Memory Game project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+Start playing by downloading the game files or cloning the directory. Once you download the game just open up the `index.html` file in your browser of choice and have fun!
 
-To get started, open `js/app.js` and start building out the app's functionality
+## How the game works
 
-For specific, detailed instructions, look at the project instructions in the [Udacity Classroom](https://classroom.udacity.com/me).
+The game board consists of sixteen cards arranged in a grid. The deck is made up of eight different pairs of cards, each with different symbols on one side. The cards are arranged randomly on the grid with the symbol face down. The gameplay rules are very simple: flip over two hidden cards at a time to locate the ones that match!
+
+Each turn:
+
+* The player flips one card over to reveal its underlying symbol.
+* The player then turns over a second card, trying to find the corresponding card with the same symbol.
+* If the cards match, both cards stay flipped over.
+* If the cards do not match, both cards are flipped face down.
+* The game ends once all cards have been correctly matched.
+
+Replay to see if you can beat your time and get a better rating!
+
+## How your rated
+
+Try to complete the game as quickly as you can, with the least amount of moves possible. This will affect your overall star rating, with 3 stars being the best, and 1 being the worst:
+
+* 3 stars - game completed in 12 moves or less
+* 2 stars - game completed in 19 moves or less
+* 1 star - game completed in 20 moves or more
 
 ## Contributing
 
-This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
-
-For details, check out [CONTRIBUTING.md](CONTRIBUTING.md).
+This is a student project for Udacity. Therefore, I most likely will not accept pull requests.
--- js/app.js
+++ js/app.js
@@ -1,38 +1,250 @@
 /*
- * Create a list that holds all of your cards
- */
+//
+// Global variables
+//
+*/
+
+//list that holds all of the cards
+const cardIcons = [
+  'fa fa-diamond', 'fa fa-diamond',
+  'fa fa-paper-plane-o', 'fa fa-paper-plane-o',
+  'fa fa-anchor', 'fa fa-anchor',
+  'fa fa-bolt', 'fa fa-bolt',
+  'fa fa-cube', 'fa fa-cube',
+  'fa fa-leaf', 'fa fa-leaf',
+  'fa fa-bicycle', 'fa fa-bicycle',
+  'fa fa-bomb', 'fa fa-bomb',
+];
+
+//selects the main board area
+const board = document.querySelector('.deck');
+
+//the arrays used to temporarily store cards
+let openedCards = [];
+let matchedCards = [];
+
+//variables for the timer
+let clockId;
+let clockOff = true;
+let minutesLabel = document.getElementById("minutes");
+let secondsLabel = document.getElementById("seconds");
+let totalSeconds = 0;
+
+//star rating variables
+const starsContainer = document.querySelector('.stars');
+let starCount;
+
+//selects the restart icon/button
+const restart = document.querySelector(".restart");
+
+//move counter variables
+const movesContainer = document.querySelector(".moves");
+let moves = 0;
 
 
 /*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+//
+// Functions
+//
+*/
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+  let currentIndex = array.length, temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+     }
+
+     return array;
+ }
+
+//Functions for the timer//
+function startClock() {
+  clockId = setInterval(setTime, 1000);
+}
+
+function setTime() {
+  ++totalSeconds;
+  secondsLabel.innerHTML = pad(totalSeconds % 60);
+  minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
+}
+
+function pad(val) {
+  let valString = val + "";
+  if (valString.length < 2) {
+    return "0" + valString;
+  } else {
+    return valString;
+  }
+}
+
+function setClockZero(){
+  totalSeconds = 0;
+  clearInterval(clockId);
+  secondsLabel.innerHTML = pad(totalSeconds % 60);
+  minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
+  clockOff = true;
+}
+
+//move counter function
+function addMove() {
+  moves++;
+  movesContainer.innerHTML = moves;
+  rating();
+}
+
+//star rating function
+function rating() {
+  if (moves > 12 && moves < 20) {
+    starsContainer.innerHTML = `<li><i class="fa fa-star"></i></li>
+    <li><i class="fa fa-star"></i></li>`;
+    starCount = 2;
+  } else if (moves >= 20) {
+    starsContainer.innerHTML = `<li><i class="fa fa-star"></i></li>`;
+    starCount = 1;
+  } else {
+    starsContainer.innerHTML = `<li><i class="fa fa-star"></i></li>
+    <li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li>`;
+    starCount = 3;
+  }
+}
+
+//start of game function
+function gameInit() {
+  shuffle(cardIcons);
+  //loops over the 'cardIcons' array and creates HTML
+  for (i = 0; i < cardIcons.length; i++) {
+    //creates an <li> tag
+    const card = document.createElement("li");
+    //adds the classname "card" to the <li> tag
+    card.classList.add("card");
+    //adds the card icons inside of the card
+    card.innerHTML = `<i class="${cardIcons[i]}"></i>`;
+    //adds the iterated card html to the board area
+    board.appendChild(card);
+    //Event Listener for card click
+    click(card);
+  }
+}
+
+//card Event Listener function
+function click(card) {
+  card.addEventListener("click", function() {
+
+    const currentCard = this;
+    const previousCard = openedCards[0];
+
+    if (clockOff) {
+      startClock();
+      clockOff = false;
     }
 
-    return array;
+    if (openedCards.length === 1) {
+      card.classList.add("open", "show", "disable");
+      openedCards.push(this);
+      //Compare the two opened cards
+      compare(currentCard, previousCard);
+    } else {
+      card.classList.add("open", "show", "disable");
+      openedCards.push(this);
+    }
+  });
 }
+
+//compare card function
+function compare(currentCard, previousCard) {
+  //compare opened cards. If they both are the same, it'll add the class name "match"
+  if (currentCard.innerHTML === previousCard.innerHTML) {
+    currentCard.classList.add("match");
+    previousCard.classList.add("match");
+    matchedCards.push(currentCard, previousCard);
+    //reset the openedCards array so that I can't put more than 2 items in it
+    openedCards = [];
+    //opens the alert modal for "GAME OVER! with a delay of 400 miliseconds"
+    setTimeout(function() {
+      gameOver();
+    }, 400);
+  //if the cards aren't the same, it'll remove the "open" & "show" classes, and reset the array
+  } else {
+      setTimeout(function() {
+        currentCard.classList.remove("open", "show", "disable");
+        previousCard.classList.remove("open", "show", "disable");
+      }, 400);
+      //resets the openedCards array so that I can't put more than 2 items in it
+      openedCards = [];
+  }
+  //adds the move function
+  addMove();
+}
+
+//game over modal function
+function gameOver() {
+    if (matchedCards.length === cardIcons.length) {
+
+        // launch modal
+        const modal = document.getElementById('myModal');
+        const span = document.getElementsByClassName("close")[0];
+        const btn = document.getElementById("myBtn");
+        modal.style.display = "block";
+
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function() {
+          modal.style.display = "none";
+        }
+
+        // When the user clicks on the button, it restarts the game
+        btn.onclick = function() {
+          resetGame();
+          modal.style.display = "none";
+        }
+
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function(event) {
+          if (event.target == modal) {
+            modal.style.display = "none";
+          }
+        }
+
+        //update the html text to reflect the game-winning stats
+        const resultsMessage = document.querySelector('.results');
+        const seconds = document.getElementById("seconds").innerHTML;
+        const minutes = document.getElementById("minutes").innerHTML;
+        resultsMessage.textContent = "Congrats, you matched all of the cards, and did so with a star rating of " + starCount + ", and with a time of " + minutes + ":" + seconds + ".";
+
+        //stop the timer
+        clearInterval(clockId);
+    }
+}
+
+//game restart function
+function resetGame() {
+    board.innerHTML = "";
+    gameInit();
+    openedCards = [];
+    matchedCards = [];
+    moves = 0;
+    movesContainer.innerHTML = moves;
+    starsContainer.innerHTML = `<li><i class="fa fa-star"></i></li>
+    <li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li>`;
+    setClockZero();
+  }
 
 
 /*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+//
+// Start of game
+//
+*/
+
+//begins the initial game
+gameInit();
+
+//game restart click event handler
+  restart.addEventListener("click", function() {
+    resetGame();
+  });
